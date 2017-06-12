403ce96086931b43fc3c6678863eb0977cdfb13a
Gennady Tanchin <g.tanchin@yandex.ru>
2017-06-12 23:35:50 +0300

    .

---
+++ inc/mqtt_codec.h
@@ -0,0 +1,28 @@
+/*
+ * msg_decod.h
+ *
+ *  Created on: 04 авг. 2016 г.
+ *      Author: Gennady Tanchin <g.tanchin@yandex.ru>
+ */
+
+#ifndef MSG_DECOD_H_
+#define MSG_DECOD_H_
+
+#include "can.h"
+
+#define TOPIC_LEVEL_MAX			3
+
+
+uint16_t mqttTopDecod( CanTxMsg *txMsg, uint8_t top[], uint16_t topLen);
+int8_t mqttMsgDecod( CanTxMsg *can, uint8_t * msg, uint8_t len, eMessId messId );
+uint8_t mqttTopCoder( uint8_t * top, CanTxMsg * can );
+uint8_t mqttMsgCoder( uint8_t * msg, CanTxMsg * can );
+
+//int32_t atoi( uint8_t *str[] );
+//uint32_t atoul( uint8_t str[] );
+
+uint8_t lToStr(int32_t l, uint8_t *str );
+uint8_t ulToStr(uint32_t l, uint8_t **str);
+uint8_t hlToStr(uint32_t l, uint8_t **str);
+
+#endif /* MSG_DECOD_H_ */
---
+++ inc/name.h
@@ -0,0 +1,29 @@
+/*
+ * name.h
+ *
+ *  Created on: 17 авг. 2016 г.
+ *      Author: Gennady Tanchin <g.tanchin@yandex.ru>
+ */
+
+#ifndef NAME_H_
+#define NAME_H_
+
+/*
+#define URL "192.168.11.4"
+#define MQTT_PORT	1883
+#define	USERNAME			"jet"
+#define	PASSWORD			"qwer1234"
+*/
+
+#define URL "m21.cloudmqtt.com"
+
+#define	USERNAME			"fsfnbhaw"
+#define	PASSWORD			"h1134SwDbKhU"
+#define MQTT_PORT			10139
+
+
+//#define		USERNAME		"bpmdvqew"
+//#define		PASSWORD		"KUS2gdM59OEY"
+
+
+#endif /* NAME_H_ */
